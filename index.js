f8a58117e2258ecbaca529eeef316fa3e322a053
Jihoon Kim <kimjigabk@gmail.com>
2019-04-19 15:56:57 +0900

    session passport auth keys and refactor

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+const keys = require("./config/keys");
+
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+});
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+//use cookies
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", (req, res) => {
-  res.send("Hellow");
+  res.send(req.user);
 });
 
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(PORT);
+});
---
+++ services/passport.js
@@ -0,0 +1,76 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+  // mongoDB에있는 user의 record의 id (구글아이디 말고)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already have a record.
+          done(null, existingUser);
+        } else {
+          // create a new User.
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
+
+// 원래만들었던 코드.
+// passport.use(User.createStrategy());
+
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser(function(id, done) {
+//   User.findById(id, function(err, user) {
+//     done(err, user);
+//   });
+// });
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.CLIENT_ID,
+//       clientSecret: process.env.CLIENT_SECRET,
+//       callbackURL: "/auth/google/secrets",
+//       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+//     },
+//     function(accessToken, refreshToken, profile, cb) {
+//       User.findOrCreate(
+//         {
+//           googleId: profile.id
+//         },
+//         {
+//           userGG: JSON.stringify(gg),
+//           userSynDataStr: ""
+//         },
+//         function(err, user) {
+//           return cb(err, user);
+//         }
+//       );
+//     }
+//   )
+// );
