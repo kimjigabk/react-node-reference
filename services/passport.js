8add3cd3ee18d8c8311f94f2d4a2913b4e4a3cac
Jihoon Kim <kimjigabk@gmail.com>
2019-04-20 13:04:45 +0900

    passport update and client setup

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // already have a record.
-          done(null, existingUser);
-        } else {
-          // create a new User.
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // already have a record.
+        return done(null, existingUser);
+      }
+      // create a new User.
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
