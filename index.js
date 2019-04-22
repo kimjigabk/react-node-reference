eeb1d177ae151249d124d04a2849c8dcc2062776
Jihoon Kim <kimjigabk@gmail.com>
2019-04-22 12:01:33 +0900

    Survey Routes/model implement

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
@@ -24,8 +25,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//route handlers
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
